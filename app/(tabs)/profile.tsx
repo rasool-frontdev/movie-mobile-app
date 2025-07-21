99977cf0e08a6189c77e292b8a5230eff12fd882
rasOol <rasool_frontdev@mail.ru>
2025-07-21 13:22:41 +0500

    Updated other pages

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { icons } from '@/constants/icons';
+import React from 'react';
+import { Image, Text, View } from 'react-native';
 
 const Profile = () => {
   return (
-    <View>
-      <Text>P</Text>
+    <View className="flex-1 bg-primary px-10">
+      <View className="flex flex-1 flex-col items-center justify-center gap-5">
+        <Image source={icons.person} tintColor={'#fff'} />
+        <Text className="text-base text-gray-500">Profile</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,12 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { icons } from '@/constants/icons';
+import React from 'react';
+import { Image, Text, View } from 'react-native';
 
 const Saved = () => {
   return (
-    <View>
-      <Text>Saved</Text>
+    <View className="flex-1 bg-primary px-10">
+      <View className="flex flex-1 flex-col items-center justify-center gap-5">
+        <Image source={icons.save} tintColor={'#fff'} />
+        <Text className="text-base text-gray-500">Profile</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Saved
+export default Saved;
