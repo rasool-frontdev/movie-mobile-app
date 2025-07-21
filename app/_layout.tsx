893f500447156670a4835e1672bafdcdfbbe4b3f
rasOol <rasool_frontdev@mail.ru>
2025-07-21 13:19:36 +0500

    Updated movie detail page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout() {
       <StatusBar hidden />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
+        <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
       </Stack>
     </>
   );
